5517feb5576f4c43f7d97ba92dcae990ed257e92
Deepa4823 <126153859+deepa4823@users.noreply.github.com>
2024-06-14 20:56:11 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,29 @@
+for i in range(1,4):
+    for j in range(1,5):
+        print("x",end=" ")
+    print()    
+
+
+
+output:
+x x x x 
+x x x x 
+x x x x
+
+
+
+
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print("x",end=" ")
+    print()
+
+
+
+
+ output
+x 
+x x 
+x x x 
+x x x x 
