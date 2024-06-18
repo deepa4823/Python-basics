14cb7f64fcafaf2906a0c3d7fe5edd926f925b93
Deepa4823 <126153859+deepa4823@users.noreply.github.com>
2024-06-18 23:36:02 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,31 @@
+#FUNCTIONS
+#write a pgm to build a login system using functions .the function name should be login and register.
+#1:it should ask user to enter the details for registration and out of all these details only user name
+#and passwd should be stored.
+#2:now this function shoukd ask user to enter username and passwd if these match with the register details
+#login successfull orelse repeat this process saying that invalid credentials.
+
+
+def regandlogin
+d={}
+print("Welcome to registration")
+    user_name=input("Enter userName:")
+    passwd=input("Enter password:")
+    name=input("Enter name")
+    phone_no=input("Enter phone no")
+    
+    d[u_name]=u_pwd
+    
+   
+    while True:
+        print("Welcome to login")
+        l_name=input("Enter login username")
+        l_pwd=input("Enter login user password
+                    
+        if l_name in d:
+                    
+             print("successfull login")
+        else:
+             print("unsuccessfull login")
+register()
+
