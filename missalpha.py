bc2983e67d497e9ffd3aa724bc3eae3a27081deb
Deepa4823 <126153859+deepa4823@users.noreply.github.com>
2024-06-19 20:36:47 +0530

    Add files via upload

---
+++ missalpha.py
@@ -0,0 +1,16 @@
+MISSING ALPHABETS
+'''
+
+n="abcdefghijklmnopqrstuvwxyz"
+s=input()
+ans=' '
+for i in n:
+    if i not in s:
+        ans+=i
+print(ans)        
+    
+
+ 
+ output:      
+welcome to geeksforgeeks
+ abdhijnpquvxyz
