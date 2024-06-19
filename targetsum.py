7fba7b20708c864a9d82ec7536fcea375878796c
Deepa4823 <126153859+deepa4823@users.noreply.github.com>
2024-06-19 20:32:21 +0530

    Add files via upload

---
+++ targetsum.py
@@ -0,0 +1,27 @@
+TARGET SUM
+
+
+
+a=list(map(int,input().split()))
+t=int(input())
+a.sort()
+i=0
+j=len(a)-1
+ans=0
+while i<j:
+    curr_sum=a[i]+a[j]
+    if curr_sum==t:
+        print(i,j)
+        i+=1
+        j-=1
+    elif curr_sum<t:
+        i=i+1
+    else:
+        j=j-1
+    
+
+
+output:
+2 3 5 7
+9
+[0 3]
